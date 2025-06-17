352e1c0b92b1331fe3a89e16d90ad6b5782fd120
18jayanth <jayanthtulugu@gmail.com>
2025-06-17 18:15:14 +0530

    Revert "Third commit:completed UI part o Google Gemini Clone"
    This reverts commit 08b1d11ed1ea76735f21e2256314d7d6c016ba74.

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,44 +9,6 @@ const Main = () => {
             <p>Gemini</p>
             <img src={assets.user_icon} alt=""/>
         </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello Dev!!</span></p>
-                <p>How Can I Help You Today!?</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest Beautiful Places to Visit on a Upcoming Road Trip</p>
-                    <img src={assets.compass_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Briefly Summarize this concept;Urban Planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>BrainStorm Team Bonding Activites For Our Work Retreat</p>
-                    <img src={assets.message_icon} alt="" />
-                </div>
-                <div className="card">
-                    <p>Tell Me about React Js and React Native</p>
-                    <img src={assets.code_icon} alt="" />
-                </div>
-            </div>
-
-            <div className="main-bottom">
-                <div className="search-box">
-                   <input type="text" placeholder='Enter a Prompt here'/> 
-                   <div>
-                    <img src={assets.gallery_icon} alt="" />
-                    <img src={assets.mic_icon}     alt="" />
-                    <img src={assets.send_icon}    alt="" />
-                   </div>
-                </div>
-                <p className="bottom-info">
-                    Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
-                </p>
-            </div>
-        </div>
       </div>
     </>
   )
